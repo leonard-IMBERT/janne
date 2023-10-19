dd794d578d6dfd9c9c2893dbf448eba3dc43bad0
Leonard Imbert <imbert@subatech.in2p3.fr>
2023-10-19 17:02:55 +0200

    [Fix] Now expose the MonitoringVars class

--- janne/trainer/__init__.py
+++ janne/trainer/__init__.py
@@ -1,6 +1,7 @@
 """Module holding the ANN training intelligence
 """
 from ._trainer import (ANNTrainingLoopConfig, ann_training_loop,
-                       ModelTrainingLoopConfig, model_training_loop)
+                       ModelTrainingLoopConfig, model_training_loop,
+                       MonitoringVars)
 
-__all__ = ["ANNTrainingLoopConfig", "ann_training_loop", "ModelTrainingLoopConfig", "model_training_loop"]
+__all__ = ["ANNTrainingLoopConfig", "ann_training_loop", "ModelTrainingLoopConfig", "model_training_loop", "MonitoringVars"]
